19d7aca496229ea141458b1e6b759c34a5550c33
AjayVishnuE <ajayvishnu121@gmail.com>
2026-01-16 01:04:42 +0530

    build files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Atul_And_Andrea/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
